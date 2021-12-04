9c58d76004a3af6b2ddf0b7852c22580b90e1e8f
arhammusheer <arhammusheer@gmail.com>
2021-12-03 23:51:52 -0500

    removed dist

--- dist/routers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (router) => {
-    router.get("/", (req, res) => {
-        res.json({});
-    });
-};
-//# sourceMappingURL=index.js.map
