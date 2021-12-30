fec0296b28adb97addb6379f94d34a6fcfcd41ab
arhammusheer <arhammusheer@gmail.com>
2021-12-30 05:10:34 -0500

    transacitons de

--- src/models/Request.model.ts
+++ src/models/Request.model.ts
@@ -1,53 +1,55 @@
 import { Document, model, Number, Schema } from "mongoose";
 
 interface Request extends Document {
-	user_id: string;
-	time: Date;
-	origin: {
-		ip: string;
-		user_agent: string;
-	}
-	transaction_id: string;
-	url: string;
-	method: string;
-	status: number;
+  user_id: string;
+  time: Date;
+  origin: {
+    ip: string;
+    user_agent: string;
+  };
+  url: string;
+  method: string;
+  status: number;
+  invoiced: boolean;
 }
 
 const RequestSchema = new Schema<Request>({
-	user_id: {
-		type: String,
-		required: true,
-	},
-	time: {
-		type: Date,
-		required: true,
-		default: new Date(Date.now()),
-	},
-	origin: {
-		ip: {
-			type: String,
-			required: true,
-		},
-		user_agent: {
-			type: String,
-			required: true,
-		}
-	},
-	transaction_id: {
-		type: String,
-	},
-	url: {
-		type: String,
-		required: true,
-	},
-	method: {
-		type: String,
-		required: true,
-	},
-	status: {
-		type: Number,
-		required: true,
-	},
+  user_id: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+    default: new Date(Date.now()),
+  },
+  origin: {
+    ip: {
+      type: String,
+      required: true,
+    },
+    user_agent: {
+      type: String,
+      required: true,
+    },
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  method: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Number,
+    required: true,
+  },
+  invoiced: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 const RequestModel = model<Request>("Request", RequestSchema);
