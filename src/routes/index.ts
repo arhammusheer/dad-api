6d6aa458379218b9aa0d8a2902b8faa722eb9caa
arhammusheer <arhammusheer@gmail.com>
2024-03-27 18:51:31 -0400

    Testing if this CI/CD is still active

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,21 +4,25 @@ import cors from "cors";
 import apiRoutes from "./api.routes";
 import authRoutes from "./auth.routes";
 import dashboardRoutes from "./dashboard.routes";
+import router from "./api.routes";
 
 const routes = Router();
 
 routes.use((req: Request, res: Response, next: NextFunction) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
-	next();
-})
-
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 routes.use("/api", apiRoutes);
 // routes.use("/auth", authRoutes);
 // routes.use("/dashboard", dashboardRoutes);
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
 
 export default routes;
