b7055179c2e162257cec7118e38bcdcf9d819858
arhammusheer <arhammusheer@gmail.com>
2024-03-27 22:51:54 +0000

    Build: (6d6aa45) Testing if this CI/CD is still active

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const api_routes_1 = __importDefault(require("./api.routes"));
+const api_routes_2 = __importDefault(require("./api.routes"));
 const routes = (0, express_1.Router)();
 routes.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,5 +15,8 @@ routes.use((req, res, next) => {
 routes.use("/api", api_routes_1.default);
 // routes.use("/auth", authRoutes);
 // routes.use("/dashboard", dashboardRoutes);
+api_routes_2.default.get("/", (req, res) => {
+    res.send("Hello World");
+});
 exports.default = routes;
 //# sourceMappingURL=index.js.map
