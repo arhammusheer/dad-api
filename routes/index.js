071cdfc5971fa5ffeefb7416c039148fb7d152b5
arhammusheer <arhammusheer@gmail.com>
2024-03-31 05:48:26 +0000

    Build: (f0cf88a) Added a rate limiter

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const express_rate_limit_1 = require("express-rate-limit");
 const api_routes_1 = __importDefault(require("./api.routes"));
 const routes = (0, express_1.Router)();
+const limiter = (0, express_rate_limit_1.rateLimit)({
+    windowMs: 60 * 1000,
+    max: 5, // limit each IP to 10 requests per windowMs
+});
+routes.use(limiter);
 routes.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
