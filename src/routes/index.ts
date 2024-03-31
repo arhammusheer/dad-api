f0cf88a729d730ac558ccfbfbec041a3b5df995d
arhammusheer <arhammusheer@gmail.com>
2024-03-31 01:48:03 -0400

    Added a rate limiter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,21 @@
 import { NextFunction, Request, Response, Router } from "express";
 import cors from "cors";
 
+import { rateLimit } from "express-rate-limit";
+
 import apiRoutes from "./api.routes";
 import authRoutes from "./auth.routes";
 import dashboardRoutes from "./dashboard.routes";
 import router from "./api.routes";
 
 const routes = Router();
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // limit each IP to 10 requests per windowMs
+});
+
+routes.use(limiter);
 
 routes.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,14 +36,13 @@ routes.get("/", (req: Request, res: Response) => {
 });
 
 routes.get("/style.css", (req: Request, res: Response) => {
-	// Serve HTML from ../tryout/index.html
-	res.sendFile("style.css", { root: "src/tryout" });
+  // Serve HTML from ../tryout/index.html
+  res.sendFile("style.css", { root: "src/tryout" });
 });
 
 routes.get("/script.js", (req: Request, res: Response) => {
-	// Serve HTML from ../tryout/index.html
-	res.sendFile("script.js", { root: "src/tryout" });
+  // Serve HTML from ../tryout/index.html
+  res.sendFile("script.js", { root: "src/tryout" });
 });
-
 
 export default routes;
